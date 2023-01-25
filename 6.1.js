32cc05ca98f7db22189cbbe3cbbe8d695012c951
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2023-01-25 17:21:44 +0000

    added 6.1

--- 6.1.js
+++ 6.1.js
@@ -7,19 +7,29 @@ function increaseBalance(x, y) {
   x -= y;
   return x;
   }
+  function increment (decreaseBalance){
+    decreaseBalance = x
+    return x;
+  }
   
   function divideBalanceByAccounts(x, y) {
     x /= y;
     return x;
-    console.log("x");
   }
   
   function getRestAfterDivision(x, y) {
     x %= y;
     return x;
   }
-  
+
+  function decrement(x) {
+    x= --x;
+    return x;
+    }
+
   console.log(increaseBalance(3000, 700));
   console.log(decreaseBalance(3000, 700));
   console.log(divideBalanceByAccounts(3000, 2));
   console.log(getRestAfterDivision(7000, 3));
+  console.log(decrement(6))
+  console.log (increment(9))
